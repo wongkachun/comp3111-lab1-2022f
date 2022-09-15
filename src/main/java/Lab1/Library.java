089bd88398f443bb030e60bd669ded94cb5e0f15
kachun <kcwongbr@connect.ust.hk>
2022-09-15 21:21:43 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,7 +7,9 @@ public class Library {
 	
 	
 	public static void main(String arg[]) {
-		System.out.println("Hello, World!");
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+			System.out.println("Hello, Word!");
 	}
 		
 	
